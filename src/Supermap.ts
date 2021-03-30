1768c02e84963c6b8736052c420000903f97fe24
Braden Lamb <octavetoast@gmail.com>
2021-03-30 00:35:09 -0400

    supermap: simple conversion to array/keyArray

--- src/Supermap.ts
+++ src/Supermap.ts
@@ -7,6 +7,9 @@ export interface SupermapConstructor {
 }
 
 export class Supermap<K, V> extends Map<K, V> {
+    private _array: V[] | null = null
+    private _keyArray: K[] | null = null
+
     constructor(entries?: ReadonlyArray<readonly [K, V]> | null) {
         super(entries)
     }
@@ -16,18 +19,34 @@ export class Supermap<K, V> extends Map<K, V> {
     }
 
     public set(key: K, value: V): this {
+        this._array = null
+        this._keyArray = null
         return super.set(key, value)
+    }
+
+    public delete(key: K): boolean {
+        this._array = null
+        this._keyArray = null
+        return super.delete(key)
     }
 
     public has(key: K): boolean {
         return super.has(key)
     }
 
-    public delete(key: K): boolean {
-        return super.delete(key)
-    }
-
     public clear(): void {
         return super.clear()
+    }
+
+    public array(): V[] {
+        if (this._array === null || this._array.length !== this.size)
+            this._array = [...this.values()]
+        return this._array
+    }
+
+    public keyArray(): K[] {
+        if (this._keyArray === null || this._keyArray.length !== this.size)
+            this._keyArray = [...this.keys()]
+        return this._keyArray
     }
 }
